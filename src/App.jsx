a29aee75b136061eaee76cb6a6a05ccb30d8b317
Nayan Man Singh Pradhan <nayan.pradhan@hotmail.com>
2025-08-03 20:21:45 +0200

    Basic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 
 function App () {
   return (
-    <div>
+    <div className="scroll-smooth">
+      <NavBar />
       <Hero />
     </div>
   );

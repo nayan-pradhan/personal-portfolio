941f5838086da109c5fac3e0fff94983e3c691b4
Nayan Man Singh Pradhan <nayan.pradhan@hotmail.com>
2025-08-05 21:18:34 +0200

    Adding experience variable

---
+++ src/components/Experience.jsx
@@ -0,0 +1,63 @@
+
+const experience = [
+    {
+        url: "https://www.tesla.com",
+        company: "Tesla",
+        roles: [
+            {
+                title: "Software Engineer, SCADA",
+                period: "2023 - Present",
+                description: "Worked on ..",
+            }
+        ]
+    },
+    {
+        company: "planblue",
+        url: "https://www.planblue.com",
+        roles: [
+            {
+                title: "Junior Development Engineer",
+                period: "2022 - 2023",
+                description: "Worked on ..",
+            }
+        ]
+    },
+    {
+        company: "Max Planck Institute for Intelligent Systems",
+        url: "https://www.is.mpg.de",
+        roles: [
+            {
+                title: "Research Assistant",
+                period: "2022 - 2022",
+                description: "Worked on ..",
+            }
+        ]
+    },
+    {
+        company: "Jacobs University Bremen",
+        url: "https://www.jacobs-university.de",
+        roles: [
+            {
+                title: "Team Lead - RAMI Underwater Marine Robotics Competition",
+                period: "2022 - 2022",
+                description: "Worked on ..",
+            },
+            {
+                title: "Research & Teaching Assistant",
+                period: "2020- 2022",
+                description: "Worked on ..",
+            }
+        ]
+    },
+];
+
+const Experience = () => {
+
+
+
+    return (
+       <></> 
+    )
+}
+
+export default Experience;
